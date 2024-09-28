5d80c19ceb103519f1db68be4d1e5fbedf995184
gavinj1999 <gavinj2006@googlemail.com>
2024-09-28 06:13:17 +0100

    vite build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	  build: {
+		outDir: path.join(__dirname, "_static")
+	  }
 });
